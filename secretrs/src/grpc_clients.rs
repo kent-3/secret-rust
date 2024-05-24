d75405cdf35761b04d579ba772af634544b9a0a6
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-05-23 23:36:53 -0400

    add IBC clients, improve readability

--- secretrs/src/grpc_clients.rs
+++ secretrs/src/grpc_clients.rs
@@ -24,29 +24,42 @@
 //!
 //! [`tonic`]: https://docs.rs/tonic
 
+use ::cosmrs::proto::cosmos;
+use ::cosmrs::proto::ibc;
+use ::secret_sdk_proto::secret;
+
 // Service Clients
-pub use ::cosmrs::proto::cosmos::base::tendermint::v1beta1::service_client::ServiceClient as TendermintServiceClient;
-pub use ::cosmrs::proto::cosmos::tx::v1beta1::service_client::ServiceClient as TxServiceClient;
+pub use cosmos::base::tendermint::v1beta1::service_client::ServiceClient as TendermintServiceClient;
+pub use cosmos::tx::v1beta1::service_client::ServiceClient as TxServiceClient;
 
 // Query Clients
-pub use ::cosmrs::proto::cosmos::auth::v1beta1::query_client::QueryClient as AuthQueryClient;
-pub use ::cosmrs::proto::cosmos::authz::v1beta1::query_client::QueryClient as AuthzQueryClient;
-pub use ::cosmrs::proto::cosmos::bank::v1beta1::query_client::QueryClient as BankQueryClient;
-pub use ::cosmrs::proto::cosmos::distribution::v1beta1::query_client::QueryClient as DistributionQueryClient;
-pub use ::cosmrs::proto::cosmos::evidence::v1beta1::query_client::QueryClient as EvidenceQueryClient;
-pub use ::cosmrs::proto::cosmos::feegrant::v1beta1::query_client::QueryClient as FeeGrantQueryClient;
-pub use ::cosmrs::proto::cosmos::gov::v1beta1::query_client::QueryClient as GovQueryClient;
-pub use ::cosmrs::proto::cosmos::mint::v1beta1::query_client::QueryClient as MintQueryClient;
-pub use ::cosmrs::proto::cosmos::params::v1beta1::query_client::QueryClient as ParamsQueryClient;
-pub use ::cosmrs::proto::cosmos::slashing::v1beta1::query_client::QueryClient as SlashingQueryClient;
-pub use ::cosmrs::proto::cosmos::staking::v1beta1::query_client::QueryClient as StakingQueryClient;
-pub use ::cosmrs::proto::cosmos::upgrade::v1beta1::query_client::QueryClient as UpgradeQueryClient;
+pub use cosmos::auth::v1beta1::query_client::QueryClient as AuthQueryClient;
+pub use cosmos::authz::v1beta1::query_client::QueryClient as AuthzQueryClient;
+pub use cosmos::bank::v1beta1::query_client::QueryClient as BankQueryClient;
+pub use cosmos::distribution::v1beta1::query_client::QueryClient as DistributionQueryClient;
+pub use cosmos::evidence::v1beta1::query_client::QueryClient as EvidenceQueryClient;
+pub use cosmos::feegrant::v1beta1::query_client::QueryClient as FeeGrantQueryClient;
+pub use cosmos::gov::v1beta1::query_client::QueryClient as GovQueryClient;
+pub use cosmos::mint::v1beta1::query_client::QueryClient as MintQueryClient;
+pub use cosmos::params::v1beta1::query_client::QueryClient as ParamsQueryClient;
+pub use cosmos::slashing::v1beta1::query_client::QueryClient as SlashingQueryClient;
+pub use cosmos::staking::v1beta1::query_client::QueryClient as StakingQueryClient;
+pub use cosmos::upgrade::v1beta1::query_client::QueryClient as UpgradeQueryClient;
 
 // Secret Query Clients
-pub use ::secret_sdk_proto::secret::compute::v1beta1::query_client::QueryClient as ComputeQueryClient;
-pub use ::secret_sdk_proto::secret::emergencybutton::v1beta1::query_client::QueryClient as EmergencyButtonQueryClient;
-pub use ::secret_sdk_proto::secret::intertx::v1beta1::query_client::QueryClient as InterTxQueryClient;
-pub use ::secret_sdk_proto::secret::registration::v1beta1::query_client::QueryClient as RegistrationQueryClient;
+
+pub use secret::compute::v1beta1::query_client::QueryClient as ComputeQueryClient;
+pub use secret::emergencybutton::v1beta1::query_client::QueryClient as EmergencyButtonQueryClient;
+pub use secret::intertx::v1beta1::query_client::QueryClient as InterTxQueryClient;
+pub use secret::registration::v1beta1::query_client::QueryClient as RegistrationQueryClient;
+
+// IBC
+pub use ibc::applications::interchain_accounts::controller::v1::query_client::QueryClient as IbcInterchainAccountsControllerQueryClient;
+pub use ibc::applications::interchain_accounts::host::v1::query_client::QueryClient as IbcInterchainAccountsHostQueryClient;
+pub use ibc::applications::transfer::v1::query_client::QueryClient as IbcTransferQueryClient;
+pub use ibc::core::channel::v1::query_client::QueryClient as IbcChannelQueryClient;
+pub use ibc::core::client::v1::query_client::QueryClient as IbcClientQueryClient;
+pub use ibc::core::connection::v1::query_client::QueryClient as IbcConnectionQueryClient;
 
 // Note: It is not possible to expose any Protobuf Msg service endpoints via gRPC.
 // Transactions must be generated and signed using the CLI or programmatically
